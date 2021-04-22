52f316fb63592f1b55079db61b1118b14afaa3d0
Rafi Abbel Mohammad <masterraf21@gmail.com>
2021-04-22 09:57:58 +0700

    add main

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/masterraf21/ecommerce-backend/apis"
 	"github.com/masterraf21/ecommerce-backend/configs"
+	"github.com/masterraf21/ecommerce-backend/usecases"
+
+	repoMongo "github.com/masterraf21/ecommerce-backend/repositories/mongodb"
+
+	"github.com/masterraf21/ecommerce-backend/utils/mongodb"
+
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/rs/cors"
@@ -22,6 +29,17 @@ type Server struct {
 	ServerReady chan bool
 }
 
+func main() {
+	instance := mongodb.ConfigureMongo()
+	serverReady := make(chan bool)
+	server := Server{
+		Instance:    instance,
+		Port:        configs.Server.Port,
+		ServerReady: serverReady,
+	}
+	server.Start()
+}
+
 // Start will start server
 func (s *Server) Start() {
 	port := configs.Server.Port
@@ -30,6 +48,22 @@ func (s *Server) Start() {
 	}
 
 	r := new(mux.Router)
+
+	counterRepo := repoMongo.NewCounterRepo(s.Instance)
+	buyerRepo := repoMongo.NewBuyerRepo(s.Instance, counterRepo)
+	sellerRepo := repoMongo.NewSellerRepo(s.Instance, counterRepo)
+	productRepo := repoMongo.NewProductRepo(s.Instance, counterRepo)
+	orderRepo := repoMongo.NewOrderRepo(s.Instance, counterRepo)
+
+	buyerUsecase := usecases.NewBuyerUsecase(buyerRepo)
+	sellerUsecase := usecases.NewSellerUsecase(sellerRepo)
+	productUsecase := usecases.NewProductUsecase(productRepo, sellerRepo)
+	orderUsecase := usecases.NewOrderUsecase(orderRepo, buyerRepo, sellerRepo, productRepo)
+
+	apis.NewBuyerAPI(r, buyerUsecase)
+	apis.NewSellerAPI(r, sellerUsecase)
+	apis.NewProductAPI(r, productUsecase)
+	apis.NewOrderAPI(r, orderUsecase)
 
 	handler := cors.Default().Handler(r)
 
